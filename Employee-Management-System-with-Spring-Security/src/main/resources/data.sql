7d6ddf2391324574694d57b3586caac63301a9f2
KritiDas1 <100513105+KritiDas1@users.noreply.github.com>
2022-07-24 23:19:32 +0530

    Create data.sql

---
+++ Employee-Management-System-with-Spring-Security/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into employee values (1001,'jaspreet@gmail.com','Jaspreet','Singh');
+insert into employee values (1002,'Rajesh@gmail.com','Rajesh','Sharma');
+insert into employee values (1003,'diksha@gmail.com','Diksha','Sharma');
+insert into employee values (1004,'Rahul@gmail.com','Rahul','Pathare');
+insert into employee values (1005,'anjali@gmail.com','Anjali','Thakur');
